901c67519b315cd8ad8d920c50859c96c4d4d955
kezzkezzkezz <60633244+kezzkezzkezz@users.noreply.github.com>
2024-12-05 14:10:25 +1300

    Update script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -1,6 +1,40 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-VERB="off"  # Fix unbound variable error
+
+function default_settings() {
+    CT_TYPE="${CT_TYPE:-1}"  # Initialize CT_TYPE
+    PW=""
+    CT_ID=$NEXTID
+    HN=$NSAPP
+    DISK_SIZE="${var_disk:-20}"
+    CORE_COUNT="${var_cpu:-2}"
+    RAM_SIZE="${var_ram:-2048}"
+    BRG="vmbr0"
+    NET="dhcp"
+    GATE=""
+    APT_CACHER=""
+    APT_CACHER_IP=""
+    DISABLEIP6="no"
+    MTU=""
+    SD=""
+    MAC=""
+    VLAN=""
+    SSH="no"
+    VERBOSE="no"
+    echo_default
+}
+
+# Define other functions
+function install_crafty() {
+    msg_info "Installing Crafty in the LXC container"
+    pct exec $CT_ID -- bash -c "apt update && apt upgrade -y"
+    pct exec $CT_ID -- bash -c "apt install -y git openjdk-21-jdk"
+    pct exec $CT_ID -- bash -c "git clone https://gitlab.com/crafty-controller/crafty-installer-4.0.git"
+    pct exec $CT_ID -- bash -c "cd crafty-installer-4.0 && ./install_crafty.sh"
+    msg_ok "Crafty successfully installed"
+}
+
+# Set up variables
 APP="Crafty"
 var_disk="20"
 var_cpu="2"
@@ -8,43 +42,12 @@ var_ram="2048"
 var_os="debian"
 var_version="12"
 
-variables          # Initialize required variables like NEXTID
-default_settings    # Set container defaults
-catch_errors        # Trap errors for better debugging
+# Execute build steps
+variables          # Initialize variables like NEXTID
+default_settings   # Ensure default_settings is defined before this call
+catch_errors       # Error handling
 
-function install_crafty() {
-    msg_info "Installing Crafty in the LXC container"
+build_container    # Build LXC
+install_crafty     # Install Crafty
 
-    # Fix locale issues
-    pct exec $CT_ID -- bash -c "apt install -y locales && locale-gen en_US.UTF-8 && update-locale LANG=en_US.UTF-8"
-
-    # Update and upgrade packages
-    pct exec $CT_ID -- bash -c "apt update && apt upgrade -y"
-
-    # Install dependencies
-    pct exec $CT_ID -- bash -c "apt install git openjdk-21-jdk -y"
-
-    # Clone Crafty repository and install
-    pct exec $CT_ID -- bash -c "git clone https://gitlab.com/crafty-controller/crafty-installer-4.0.git"
-    pct exec $CT_ID -- bash -c "cd crafty-installer-4.0 && ./install_crafty.sh"
-
-    msg_ok "Crafty successfully installed"
-}
-
-function get_ip() {
-    msg_info "Retrieving the container's IP address"
-    IP=$(pct exec $CT_ID -- hostname -I | awk '{print $1}')
-    if [[ -z "$IP" ]]; then
-        msg_error "Failed to retrieve the container's IP address"
-        exit 1
-    fi
-    msg_ok "IP Address: $IP"
-}
-
-build_container      # Create the LXC container
-install_crafty       # Install Crafty
-get_ip               # Retrieve and display the IP address
-
-msg_ok "Completed Successfully!\n"
-echo -e "${APP} should be reachable by going to the following URL:
-         http://${IP}:8000 \n"
+msg_ok "Completed Successfully!"
