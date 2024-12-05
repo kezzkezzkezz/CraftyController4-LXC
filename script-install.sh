41fd6d9fd769b570107762b686f5ee7744e371a4
kezzkezzkezz <60633244+kezzkezzkezz@users.noreply.github.com>
2024-12-05 14:21:06 +1300

    Update script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck
-# Co-Author: MickLesk (Canbiz)
-# License: MIT
-# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://github.com/matze/wastebin
+# Source: https://github.com/community-scripts/
 # Function definitions
 
 function header_info {
