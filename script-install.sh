32cb4f471b6ee2b0629f3e30e3b215941790d095
kezzkezzkezz <60633244+kezzkezzkezz@users.noreply.github.com>
2024-12-05 14:09:30 +1300

    Update script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -1,102 +1,50 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
-# License: MIT
-# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-
-function header_info {
-clear
-cat <<"EOF"
-  ____            __ _            ____            _             _ _             _  _   
- / ___|_ __ __ _ / _| |_ _   _   / ___|___  _ __ | |_ _ __ ___ | | | ___ _ __  | || |  
-| |   | '__/ _` | |_| __| | | | | |   / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__| | || |_ 
-| |___| | | (_| |  _| |_| |_| | | |__| (_) | | | | |_| | | (_) | | |  __/ |    |__   _|
- \____|_|  \__,_|_|  \__|\__, |  \____\___/|_| |_|\__|_|  \___/|_|_|\___|_|       |_|  
-                         |___/                                                         
-EOF
-}
-header_info
-echo -e "Loading..."
+VERB="off"  # Fix unbound variable error
 APP="Crafty"
 var_disk="20"
 var_cpu="2"
 var_ram="2048"
 var_os="debian"
 var_version="12"
-variables
-color
-catch_errors
 
-function default_settings() {
-    CT_TYPE="1"
-    PW=""
-    CT_ID=$NEXTID
-    HN=$NSAPP
-    DISK_SIZE="$var_disk"
-    CORE_COUNT="$var_cpu"
-    RAM_SIZE="$var_ram"
-    BRG="vmbr0"
-    NET="dhcp"
-    GATE=""
-    APT_CACHER=""
-    APT_CACHER_IP=""
-    DISABLEIP6="no"
-    MTU=""
-    SD=""
-    MAC=""
-    VLAN=""
-    SSH="no"
-    VERBOSE="no"
-    echo_default
-}
+variables          # Initialize required variables like NEXTID
+default_settings    # Set container defaults
+catch_errors        # Trap errors for better debugging
+
 function install_crafty() {
     msg_info "Installing Crafty in the LXC container"
 
     # Fix locale issues
     pct exec $CT_ID -- bash -c "apt install -y locales && locale-gen en_US.UTF-8 && update-locale LANG=en_US.UTF-8"
 
-    # Update and upgrade packages in the container
+    # Update and upgrade packages
     pct exec $CT_ID -- bash -c "apt update && apt upgrade -y"
 
-    # Install necessary dependencies: Git and OpenJDK 21
+    # Install dependencies
     pct exec $CT_ID -- bash -c "apt install git openjdk-21-jdk -y"
 
-    # Clone Crafty installer repository
+    # Clone Crafty repository and install
     pct exec $CT_ID -- bash -c "git clone https://gitlab.com/crafty-controller/crafty-installer-4.0.git"
-
-    # Change directory and run the install script
-    pct exec $CT_ID -- bash -c "cd crafty-installer-4.0 && sudo ./install_crafty.sh"
+    pct exec $CT_ID -- bash -c "cd crafty-installer-4.0 && ./install_crafty.sh"
 
     msg_ok "Crafty successfully installed"
 }
 
-function start_crafty() {
-    msg_info "Starting Crafty application"
-    pct exec $CT_ID -- bash -c "systemctl start crafty"
-    msg_ok "Crafty has started"
+function get_ip() {
+    msg_info "Retrieving the container's IP address"
+    IP=$(pct exec $CT_ID -- hostname -I | awk '{print $1}')
+    if [[ -z "$IP" ]]; then
+        msg_error "Failed to retrieve the container's IP address"
+        exit 1
+    fi
+    msg_ok "IP Address: $IP"
 }
 
-function update_crafty() {
-    msg_info "Updating Crafty in the LXC container"
-    pct exec $CT_ID -- bash -c "cd /opt/crafty && git pull origin master"
-    pct exec $CT_ID -- bash -c "systemctl restart crafty"
-    msg_ok "Crafty has been updated and restarted"
-}
-
-start
-build_container
-default_settings
-description
-
-# Install Crafty
-install_crafty
-
-# Optionally, update Crafty if needed
-# update_crafty
-
-start_crafty
+build_container      # Create the LXC container
+install_crafty       # Install Crafty
+get_ip               # Retrieve and display the IP address
 
 msg_ok "Completed Successfully!\n"
-echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}:8000${CL} \n"
+echo -e "${APP} should be reachable by going to the following URL:
+         http://${IP}:8000 \n"
