476b9929399e9eadc4d927a19e21256a4afc89dd
kezzkezzkezz <60633244+kezzkezzkezz@users.noreply.github.com>
2024-12-05 14:19:41 +1300

    Update script-install.sh

--- script-install.sh
+++ script-install.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
-
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-
+# Copyright (c) 2021-2024 tteck
+# Author: tteck
+# Co-Author: MickLesk (Canbiz)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/matze/wastebin
 # Function definitions
-function default_settings() {
-    CT_TYPE="${CT_TYPE:-1}"  # Set default container type
-    PW=""
-    CT_ID=$NEXTID
-    HN=$NSAPP
-    DISK_SIZE="${var_disk:-20}"
-    CORE_COUNT="${var_cpu:-2}"
-    RAM_SIZE="${var_ram:-2048}"
-    BRG="vmbr0"
-    NET="dhcp"
-    GATE=""
-    APT_CACHER=""
-    APT_CACHER_IP=""
-    DISABLEIP6="no"
-    MTU=""
-    SD=""
-    MAC=""
-    VLAN=""
-    SSH="no"
-    VERBOSE="no"
-    echo_default
+
+function header_info {
+clear
+cat <<"EOF"
+  ____            __ _            ____            _             _ _             _  _   
+ / ___|_ __ __ _ / _| |_ _   _   / ___|___  _ __ | |_ _ __ ___ | | | ___ _ __  | || |  
+| |   | '__/ _` | |_| __| | | | | |   / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__| | || |_ 
+| |___| | | (_| |  _| |_| |_| | | |__| (_) | | | | |_| | | (_) | | |  __/ |    |__   _|
+ \____|_|  \__,_|_|  \__|\__, |  \____\___/|_| |_|\__|_|  \___/|_|_|\___|_|       |_|  
+                         |___/                                                         
+EOF
 }
+header_info
+echo -e "Loading..."
+APP="Wastebin"
+var_disk="4"
+var_cpu="1"
+var_ram="1024"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
 
 function install_crafty() {
     msg_info "Installing Crafty in the LXC container"
@@ -35,10 +40,12 @@ function install_crafty() {
     msg_ok "Crafty successfully installed"
 }
 
-# Main script execution
-variables            # Load default variables
-default_settings     # Call after function is defined
-build_container      # Build LXC
-install_crafty       # Install Crafty
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} Setup should be reachable by going to the following URL.
+         ${BL}http://${IP}:8088${CL} \n"
 
 msg_ok "Completed Successfully!"
